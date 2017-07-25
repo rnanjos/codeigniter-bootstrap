8924050923a661bbcef6689909254a4fe9e4f0df
Renato <renatu.anjos@gmail.com>
2017-07-25 20:02:06 -0300

    Views

--- application/views/index.php
+++ application/views/index.php
@@ -2,10 +2,14 @@
 <?php
 $this->load->view('layout/header');
 $this->load->view('layout/nav');
+$this->load->view('layout/slider');
+
+/* 
+Condição para não exibir o slides nas outras paginas
 if ($view_principal == 'home') {
 	$this->load->view('layout/slider');
-	
 }
+*/
 $this->load->view($view_principal);
 $this->load->view('layout/footer');
 ?>
